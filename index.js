7ed0244447ad68b114f83c5aafb6796e92763e59
ItsMeSparkY <119124402+ItsMeSparkY@users.noreply.github.com>
2024-01-27 16:46:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
+const fs = require('fs');
 const discord = require('freeze-selfbot');
 const client = new discord.Client();
-const config = require("./config")
 const express = require('express');
-const console = require("./utils/logger")
+const console = require("./utils/logger");
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Read the token from the data.txt file
+const TOKEN = fs.readFileSync('data.txt', 'utf-8').trim();
+
+if (!TOKEN) {
+  console.exit("No token provided");
+}
+
+const MODE = process.env.MODE || 'default';
 
 module.exports = client;
-process.env.TOKEN ? client.login(process.env.TOKEN) : console.exit("No token provided");
-config.mode ? require(`./presences/${config.mode}`) : console.exit('No presence selected!');
-console.logger.info(`
 
-                    ██████╗ ██╗███████╗ ██████╗ ██████╗ ██████╗ ██████╗       ██████╗ ██████╗  ██████╗
-                    ██╔══██╗██║██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔══██╗      ██╔══██╗██╔══██╗██╔════╝
-                    ██║  ██║██║███████╗██║     ██║   ██║██████╔╝██║  ██║█████╗██████╔╝██████╔╝██║     
-                    ██║  ██║██║╚════██║██║     ██║   ██║██╔══██╗██║  ██║╚════╝██╔══██╗██╔═══╝ ██║     
-                    ██████╔╝██║███████║╚██████╗╚██████╔╝██║  ██║██████╔╝      ██║  ██║██║     ╚██████╗
-                    ╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝       ╚═╝  ╚═╝╚═╝      ╚═════╝                                                                                
-`
-)
+client.login(TOKEN);
+
+console.logger.info(`
+   ██████╗ ██╗███████╗ ██████╗ ██████╗ ██████╗ ██████╗       ██████╗ ██████╗  ██████╗
+   ██╔══██╗██║██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔══██╗      ██╔══██╗██╔══██╗██╔════╝
+   ██║  ██║██║███████╗██║     ██║   ██║██████╔╝██║  ██║█████╗██████╔╝██████╔╝██║     
+   ██║  ██║██║╚════██║██║     ██║   ██║██╔══██╗██║  ██║╚════╝██╔══██╗██╔═══╝ ██║     
+   ██████╔╝██║███████║╚██████╗╚██████╔╝██║  ██║██████╔╝      ██║  ██║██║     ╚██████╗
+   ╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝       ╚═╝  ╚═╝╚═╝      ╚═════╝                                                                                
+`);
 
 app.get('/', (_, res) => {
   res.send('Presence is ready!');
 });
-app.listen(3000)
+
+app.listen(PORT, () => {
+  console.logger.info(`Web server is running on port ${PORT}`);
+});
